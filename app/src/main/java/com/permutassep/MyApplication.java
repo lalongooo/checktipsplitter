a229909ed4eea9106f9761f0983ca17e4bc2d16d
lalongooo <hdez.jeduardo@gmail.com>
2015-07-14 13:25:01 -0500

    Removed unused res/classes

--- app/src/main/java/com/permutassep/MyApplication.java
+++ app/src/main/java/com/permutassep/MyApplication.java
@@ -1,14 +1,6 @@
 package com.permutassep;
 
 import android.app.Application;
-import android.content.Context;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.widget.ImageView;
-
-import com.facebook.FacebookSdk;
-import com.mikepenz.materialdrawer.util.DrawerImageLoader;
-import com.squareup.picasso.Picasso;
 
 public class MyApplication extends Application {
 
@@ -19,24 +11,5 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FacebookSdk.sdkInitialize(getApplicationContext());
-
-        //initialize and create the image loader logic
-        DrawerImageLoader.init(new DrawerImageLoader.IDrawerImageLoader() {
-            @Override
-            public void set(ImageView imageView, Uri uri, Drawable placeholder) {
-                Picasso.with(imageView.getContext()).load(uri).placeholder(placeholder).into(imageView);
-            }
-
-            @Override
-            public void cancel(ImageView imageView) {
-                Picasso.with(imageView.getContext()).cancelRequest(imageView);
-            }
-
-            @Override
-            public Drawable placeholder(Context ctx) {
-                return null;
-            }
-        });
     }
 }
