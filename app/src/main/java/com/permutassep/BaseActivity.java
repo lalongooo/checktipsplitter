de3adae3da9bee4872a0bc517b927669bed3f1e2
lalongooo <hdez.jeduardo@gmail.com>
2015-07-14 11:39:36 -0500

    Removed unused res/classes

--- app/src/main/java/com/permutassep/BaseActivity.java
+++ app/src/main/java/com/permutassep/BaseActivity.java
@@ -3,28 +3,10 @@ package com.permutassep;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.analytics.HitBuilders;
-import com.google.android.gms.analytics.Tracker;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        if(GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext()) == 0) {
-            // Get tracker.
-            Tracker t = ((PermutasSEPApplication) getApplication()).getTracker();
-            // Set screen name.
-            t.setScreenName(getClass().getName());
-            // Send a screen view.
-            t.send(new HitBuilders.ScreenViewBuilder().build());
-        }
     }
 }
--- app/src/main/java/com/permutassep/MyApplication.java
+++ app/src/main/java/com/permutassep/MyApplication.java
@@ -4,20 +4,15 @@ import android.app.Application;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.os.Build;
 import android.widget.ImageView;
 
 import com.facebook.FacebookSdk;
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.Logger;
-import com.google.android.gms.analytics.Tracker;
-import com.lalongooo.permutassep.BuildConfig;
 import com.mikepenz.materialdrawer.util.DrawerImageLoader;
 import com.squareup.picasso.Picasso;
 
-public class PermutasSEPApplication extends Application {
+public class MyApplication extends Application {
 
-    public PermutasSEPApplication() {
+    public MyApplication() {
         super();
     }
 
@@ -43,17 +38,5 @@ public class PermutasSEPApplication extends Application {
                 return null;
             }
         });
-    }
-
-    /**
-     * Retrieves the Tracker object to send hits to of Google Analytics
-     * */
-    public synchronized Tracker getTracker() {
-        GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-        analytics.setDryRun(BuildConfig.DEBUG);
-        analytics.getLogger().setLogLevel(Logger.LogLevel.VERBOSE);
-        Tracker t = analytics.newTracker(BuildConfig.google_analytics_property_id);
-        t.enableAdvertisingIdCollection(true);
-        return t;
     }
 }
